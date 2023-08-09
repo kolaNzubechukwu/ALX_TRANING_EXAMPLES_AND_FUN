d9b27d249b8fb0649732d68d546cef157a8a00b5
kolaNzubechukwu <fransiscomoma@gmail.com.com>
2023-08-08 18:03:11 -0700

    commited password

---
+++ TESTS/password_verificatoion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+int checkpassword(char *password);
+
+int main()
+{
+	char password[50];
+	char correct_password[] = "fransisco";
+	printf("enter a password \"there should be no space maximum character of 50\": ");
+	scanf("%s", password);
+ 	int i = checkpassword(password);
+	if (i == 1) 
+		printf("password :%s ✅\n", password);
+	return (0);
+}
+
+int checkpassword(char *password)
+{
+	char correct_password[] = "fransiscz";
+	for (int i = 0; i <= strlen(correct_password) - 1; i++)
+	{
+		if (password[i] != correct_password[i])
+			return 0;
+	}
+	return 1;
+}
